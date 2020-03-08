730fe306a7cc57401e3b5c61e0ecaa1dbe917b76
kg534 <kg-katsu08@i.softbank.jp>
2020-03-08 21:50:39 +0900

    delete product function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,8 +12,17 @@ class ProductsController < ApplicationController
 
   def create
     @category = Category.find_by(id: category_params[:category_id])
-    @product = Product.new(product_params)
+    brand = brand_params[:brand_attributes]
+    @brand = Brand.find_by(name: brand[:name])
 
+    if brand[:name] == "" 
+      @product = Product.new(product_no_brand_params)
+    elsif @brand != nil
+      @product = Product.new(product_params_brand_id)
+    else
+      @product = Product.new(product_params)
+    end
+    
     if @product.save
       redirect_to root_path
     else  
@@ -29,15 +38,25 @@ class ProductsController < ApplicationController
 
   def update
     @category = Category.find_by(id: category_params[:category_id])
-    
-    if @product.update(product_params) 
+    brand = brand_params[:brand_attributes]
+    @brand = Brand.find_by(name: brand[:name])
+
+    if brand[:name] == ""  
+      @product.update(product_no_brand_params) 
       redirect_to root_path
-    else 
+    elsif @brand != nil
+      @product.update(product_params_brand_id)
+      redirect_to root_path
+    elsif @product.update(product_params) 
+      redirect_to root_path
+    else
       render :edit
     end
   end
 
   def destroy
+    @product.destroy
+    redirect_to root_path
   end
 
   private
@@ -49,7 +68,19 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:category_id)
   end
 
+  def brand_params
+    params.require(:product).permit(brand_attributes: [:id, :name])
+  end
+
+  def product_no_brand_params
+    params.require(:product).permit(:name, :detail, :condition, :shopping_charges, :delivery_area, :delivery_date, :price, :status, images_attributes: [:src, :_destroy, :id]).merge(user_id: current_user.id, category_id: @category.id, brand_id: nil)
+  end
+
   def product_params
     params.require(:product).permit(:name, :detail, :condition, :shopping_charges, :delivery_area, :delivery_date, :price, :status, images_attributes: [:src, :_destroy, :id], brand_attributes: [:id, :name]).merge(user_id: current_user.id, category_id: @category.id)
+  end
+
+  def product_params_brand_id
+    params.require(:product).permit(:name, :detail, :condition, :shopping_charges, :delivery_area, :delivery_date, :price, :status, images_attributes: [:src, :_destroy, :id]).merge(user_id: current_user.id, category_id: @category.id, brand_id: @brand[:id])
   end
 end
