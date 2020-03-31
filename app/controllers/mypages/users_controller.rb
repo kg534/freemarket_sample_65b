a14a72d42927617ca43986c254473ff35da2c3d2
Omitsu1218 <mitsunaga1218@gmail.com>
2020-03-31 22:57:18 +0900

    マイページ実装
    検索機能も追加（曖昧検索可）

---
+++ app/controllers/mypages/users_controller.rb
@@ -0,0 +1,5 @@
+class Mypages::UsersController < ApplicationController
+  def show
+    # @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+  namespace :mypages do
+    resources :users, only: :show
+  end
   root to: "products#index"
   resources :products do
     collection do
       get 'search'
     end
   end
-  resources :users, only: :show
 end
