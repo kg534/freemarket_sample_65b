796b9508248784676665a13e2134ecee1dd86ccc
eiryu04 <legendwing0403@gmail.com>
2020-04-19 19:52:46 +0900

    カテゴリー 修正
    不必要なコメントアウト消しました
    ハッシュロケットをシンボルに修正しました

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_31_142034) do
-  
+
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
     t.integer "prefecture", null: false
