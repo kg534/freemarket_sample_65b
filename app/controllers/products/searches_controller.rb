698750abead79cf57346a8223ffc1f7366ae7c4c
Omitsu1218 <mitsunaga1218@gmail.com>
2020-05-03 01:40:53 +0900

    検索機能　売り切れ商品　非表示
    searches_controller.rbのproductsにwhereメソッドを追加

--- app/controllers/products/searches_controller.rb
+++ app/controllers/products/searches_controller.rb
@@ -1,6 +1,6 @@
 class Products::SearchesController < ApplicationController 
   def index
     products = Product.search(params[:keyword])
-    @products = products.includes(:images).order("created_at DESC").page(params[:page]).per(6)
+    @products = products.where(status:0).includes(:images).order("created_at DESC").page(params[:page]).per(6)
   end
 end
